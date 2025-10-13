3e014cee3901acc870b9145bbabe1fd57484ec1c
standardhub <199046031+standardhub@users.noreply.github.com>
2025-10-13 05:52:02 -0700

    feat:server

---
+++ backend/server.js
@@ -0,0 +1,47 @@
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import mongoose from "mongoose";
+import cors from "cors";
+import jwt from "jsonwebtoken";
+import { typeDefs } from "./schema.js";
+import { resolvers } from "./resolvers.js";
+import { MONGO_URI, JWT_SECRET } from "./config.js"
+
+const app = express();
+app.use(cors());
+
+const getUser = (token) => {
+    try {
+        if (token) {
+            return jwt.verify(token, JWR_)
+        }
+        return null;
+    } catch {
+        return null;
+    }
+};
+
+async function startServer() {
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+        context: ({ req }) => {
+            const token = req.headers.authorization || "";
+            const user = getUser(token.replace("Bear ", ""));
+            return { user };
+        }
+    });
+
+    await server.start();
+    server.applyMiddleware({ app });
+
+    mongoose.connect(MONGO_URI)
+        .then(() => console.log("MongoDB Connected"))
+        .catch(err => console.error(err));
+
+    app.listen(4000, () => {
+        console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+    })
+}
+
+startServer();
