459932c840844a548322a3dcb6d0b251737ec94e
standardhub <199046031+standardhub@users.noreply.github.com>
2025-10-14 00:58:19 -0700

    feat:mutations

---
+++ frontend/src/graphql/mutations.js
@@ -0,0 +1,23 @@
+import { gql } from "@apollo/client";
+
+export const REGISTER_USER = gql`
+    mutation Register($name: String!, $email: String!, $password:String!) {
+        register(name: $name, email: $email, password: $password) {
+            id
+            name
+            email
+            token
+        }
+    }
+`;
+
+export const LOGIN_USER = gql`
+    mutation Login($email: String!, $password: String!) {
+        login(email: $email, passowrd: $password) {
+            id
+            name
+            email
+            token
+        }
+    }
+`;
